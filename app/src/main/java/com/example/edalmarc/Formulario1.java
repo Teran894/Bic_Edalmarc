94ca5e2c77b0d24871bd0f424be6ca815f70a0c3
Jose Luis Teran Soto <superviper.jlts@gmail.com>
2022-11-19 12:52:11 -0700

    Cambios a formulario, ya funciona excepto la firma
    Falta configurar el boton de la imagen y el apartado de firmar. Pero ya todos los demas campos funcionan.

--- app/src/main/java/com/example/edalmarc/Formulario1.java
+++ app/src/main/java/com/example/edalmarc/Formulario1.java
@@ -4,26 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Formulario1 extends AppCompatActivity {
-    private Button button;
+    private Button gotoformulario2;
+    private EditText editextHoraInicial, editextNombreCliente, editextTelefonoCliente, editextDireccionCliente, editextTipoTecnico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario1);
-        button = (Button) findViewById(R.id.btnInicioSesion);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        editextHoraInicial = (EditText) findViewById(R.id.TiempoDeEntrada);
+        editextNombreCliente = (EditText) findViewById(R.id.NombreCliente);
+        editextTelefonoCliente = (EditText) findViewById(R.id.telefonoCliente);
+        editextDireccionCliente = (EditText)findViewById(R.id.direccionCliente);
+        editextTipoTecnico = (EditText) findViewById(R.id.tipoTecnico);
+
+
+        gotoformulario2 = (Button) findViewById(R.id.GotoFormulario2);
+        gotoformulario2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openInicio();
             }
         });
+
+        editextHoraInicial.addTextChangedListener(loginTextWatcher);
+        editextNombreCliente.addTextChangedListener(loginTextWatcher);
+        editextTelefonoCliente.addTextChangedListener(loginTextWatcher);
+        editextDireccionCliente.addTextChangedListener(loginTextWatcher);
+        editextTipoTecnico.addTextChangedListener(loginTextWatcher);
+
+
     }
+
+
     public void openInicio(){
         Intent intent = new Intent(this, formulario2.class);
         startActivity(intent);
     }
+
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int count, int after) {
+            String HoraInicial = editextHoraInicial.getText().toString().trim();
+            String NombreCliente = editextNombreCliente.getText().toString().trim();
+            String TelefonoCliente = editextTelefonoCliente.getText().toString().trim();
+            String DireccionCliente = editextDireccionCliente.getText().toString().trim();
+            String TipoTecnico = editextTipoTecnico.getText().toString().trim();
+
+
+            gotoformulario2.setEnabled(!HoraInicial.isEmpty() && !NombreCliente.isEmpty() && !TelefonoCliente.isEmpty() && !DireccionCliente.isEmpty() && !TipoTecnico.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
+
 }
--- app/src/main/java/com/example/edalmarc/MainActivity.java
+++ app/src/main/java/com/example/edalmarc/MainActivity.java
@@ -4,25 +4,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button login, showHideBtn;
+    private EditText editextUsername;
+    private EditText editextPassword;
+    private CheckBox showPSSWD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.btnInicioSesion);
-        button.setOnClickListener(new View.OnClickListener() {
+        editextUsername = findViewById(R.id.editTextUserName);
+        editextPassword = findViewById(R.id.editTextPassword);
+
+        showPSSWD = (CheckBox) findViewById(R.id.showPSSWD);
+        showPSSWD.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    editextPassword.setTransformationMethod((HideReturnsTransformationMethod.getInstance()));
+                } else {
+                    editextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        login = (Button) findViewById(R.id.btnInicioSesion);
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openInicio();
             }
         });
+        editextUsername.addTextChangedListener(loginTextWatcher);
+        editextPassword.addTextChangedListener(loginTextWatcher);
     }
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int count, int after) {
+            String usernameInput = editextUsername.getText().toString().trim();
+            String passwordInput = editextPassword.getText().toString().trim();
+
+            login.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
     public void openInicio(){
         Intent intent = new Intent(this, Formulario1.class);
         startActivity(intent);
